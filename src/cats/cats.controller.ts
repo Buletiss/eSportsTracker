43da65a3f0f8a2d25195e2148f1e6b3e07a7322a
Buletiss <vinicius1997feitosa@gmail.com>
2023-05-25 00:53:29 -0300

    chore: :fire: remoção dos arquivos .cats

--- src/cats/cats.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Controller, Get, Req } from "@nestjs/common";
-
-@Controller("cats")
-export class CatsController {
-  @Get()
-  findAll(): string {
-    return "This action returns all cats";
-  }
-}
