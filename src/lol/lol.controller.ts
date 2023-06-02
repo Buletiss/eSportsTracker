4483b7a6dd4e849b02e5ee01fbe3a477246ed3ec
Buletiss <vinicius1997feitosa@gmail.com>
2023-06-02 11:56:55 -0300

    feat: :sparkles: query rota schedule lol

--- src/lol/lol.controller.ts
+++ src/lol/lol.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Req } from "@nestjs/common";
+import { Controller, Get, Req, Query } from "@nestjs/common";
 import { LolService } from "./lol.service";
 
 @Controller("lol")
@@ -7,7 +7,25 @@ export class LolsController {
     this.LolService = LolService;
   }
   @Get()
-  findAll() {
-    return this.LolService.findAll();
+  //rota com query  'lol?page=1'
+  async findAll(@Query("page") page: number) {
+    const allLols = await this.LolService.findAll();
+    const limitPerPage = 10;
+    const startIndex = (page - 1) * limitPerPage;
+    const endIndex = page * limitPerPage;
+
+    const paginatedLols = allLols.slice(startIndex, endIndex);
+
+    const totalMatch = allLols.length;
+
+    if (paginatedLols != null) {
+      return {
+        data: paginatedLols.length ? paginatedLols : "Pagina Não encontrada",
+        totalMatch: totalMatch,
+        maxPages: Math.ceil(totalMatch / limitPerPage),
+      };
+    } else {
+      return { data: "send nullable text" };
+    }
   }
 }
--- src/lol/lol.service.ts
+++ src/lol/lol.service.ts
@@ -10,17 +10,13 @@ export class LolService {
   }
 
   async findAll() {
-    const url =
-      "https://esports-api.lolesports.com/persisted/gw/getSchedule?hl=pt-BR";
-    const apiKey =
-      "0TvQnueqKa5mxJntVWt0w4LpLfEkrV1Ta8rQBb9Z";
+    const url = "https://esports-api.lolesports.com/persisted/gw/getSchedule?hl=pt-BR";
+    const apiKey = "0TvQnueqKa5mxJntVWt0w4LpLfEkrV1Ta8rQBb9Z";
 
     const headers = { "x-api-key": apiKey };
 
     const response = await lastValueFrom(
-      this.httpService
-        .get(url, { headers })
-        .pipe(map(resp => resp.data.data.schedule.events)),
+      this.httpService.get(url, { headers }).pipe(map(resp => resp.data.data.schedule.events)),
     );
 
     var responseMap = response.map(e => {
